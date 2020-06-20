78c736b3bd17c11716bbc2def03eb57a904fbf7b
Farkhad Muminov <libreno@gmail.com>
2020-06-20 23:54:05 +0400

    removed unused package react-hook; added request scheduling 3 per second.

--- src/services/VKService.js
+++ src/services/VKService.js
@@ -1,5 +1,4 @@
 import bridge from "@vkontakte/vk-bridge";
-import useThrottleCallback from '@react-hook/throttle';
 
 const API_VERSION = "5.110";
 const FRIENDS_GET_REQEST_ID = "friends.get";
@@ -7,8 +6,12 @@ const GROUPS_GET_REQEST_ID = "groups.get";
 // const GROUPS_GET_BY_ID_REQUEST_ID = "groups.getById";
 
 let token = null;
+let friendsCount = 0;
+let scheduledTime_ms = 0;
 
-let callAPIMethod = (method, requestId, params) => { 
+const requestInterval_ms = 334;
+
+const callAPI = (method, requestId, params) => { 
     let request = {
         "method": method, 
         "request_id": requestId, 
@@ -16,31 +19,59 @@ let callAPIMethod = (method, requestId, params) => {
     };
     params["v"] = API_VERSION;
     params["access_token"] = token;
-    bridge.send("VKWebAppCallAPIMethod", request);
+
+    // first call runs without delay
+    if (scheduledTime_ms === 0){
+        bridge.send("VKWebAppCallAPIMethod", request);
+        scheduledTime_ms = Date.now();
+    }
+    else{
+        // limit 3 requests per second
+        scheduledTime_ms = Math.max(scheduledTime_ms + requestInterval_ms, Date.now());
+        let timeout = scheduledTime_ms - Date.now();
+        setTimeout(() => {
+            bridge.send("VKWebAppCallAPIMethod", request);
+        }, timeout);
+        console.log(`wait ${timeout} ms`);
+    }
 };
 
-let getGroupsData = (userId = "1") => { 
-    let onTokenReceived = ({ detail: { type, data }}) => {
+const getGroupsData = (progress, userId = "1") => { 
+    const onTokenReceived = ({ detail: { type, data }}) => {
         if (type === "VKWebAppAccessTokenReceived") {
             token = data.access_token;
             // console.log(`token ${token}`);
             bridge.unsubscribe(onTokenReceived);
-            callAPIMethod("friends.get", FRIENDS_GET_REQEST_ID, { "userIds": userId });
+            callAPI("friends.get", FRIENDS_GET_REQEST_ID, { "userIds": userId });
         }
     };
     bridge.subscribe(onTokenReceived);
     let groupsData = new Map();
-    let onFriendsDataReceived = ({ detail: { type, data }}) => {
+    let friendsDataReceived = 0;
+    async function onFriendsDataReceived({ detail: { type, data }}) {
         if (type === "VKWebAppCallAPIMethodResult" && data.request_id === FRIENDS_GET_REQEST_ID){
             bridge.unsubscribe(onFriendsDataReceived);
             // console.log('flat');
             // console.log(data.response.items.flat());
-            let onGroupsDataReceived = ({ detail: { type, data }}) => {
+            const onGroupsDataReceived = ({ detail: { type, data }}) => {
+                console.log({ detail: { type, data }});
+                if (type === "VKWebAppCallAPIMethodFailed" && data.error_data.error_reason.error_code === 6){
+                    // In case of error: too many requests per second - reschedule the request
+                    let params = data.error_data.error_reason.request_params;
+                    callAPI("groups.get", params.find(p => p.key === "request_id").value, params);
+                }
+                else{
+                    progress = ++friendsDataReceived * 100 / friendsCount;
+                };
                 if (type === "VKWebAppCallAPIMethodResult" && data.request_id.startsWith(GROUPS_GET_REQEST_ID)){
                     let requestedUserId = data.request_id.split(' ')[1];
                     console.log(`requestedUserId ${requestedUserId}`);
-                    console.log(data);
-                    if (!!data.items){
+                    if (!!data.response.items){
+                        // progress = ++friendsDataReceived * 100 / friendsCount;
+                        console.log(`progress ${progress}`);
+                        // let key = `${g.name}-${g.id}`;
+                        // let val = groupsData.get(key);
+                        // groupsData.set(key, val === undefined? 1: ++val);
                         // console.log(`No groups for user ${requestedUserId}`);
                         // let ids = [];
                         // data.items.forEach(groupId => {
@@ -64,8 +95,10 @@ let getGroupsData = (userId = "1") => {
             bridge.subscribe(onGroupsDataReceived);
             // todo: unsubscribe from onUsersDataReceived after all users data received
             // console.log(data.response);
-            data.response.items.flat().forEach(userId => {
-                callAPIMethod("groups.get", `${GROUPS_GET_REQEST_ID} ${userId}`, { "user_id":  userId, "extended": 1 });
+            let friends = data.response.items.flat();
+            friendsCount = friends.length;
+            friends.forEach(userId => {
+                callAPI("groups.get", `${GROUPS_GET_REQEST_ID} ${userId}`, { "user_id":  userId, "extended": 1 });
             });
         }
     };
